f79cce903c2897e472e32533ccecf1d656cce867
Tanyi mary <tabi.tanyi@student.nhlstenden.com>
2025-11-18 05:31:41 +0100

    commit changes

--- index.php
+++ index.php
@@ -27,9 +27,16 @@ unset($lang_options[$current_lang]);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Welcome to cameroon</title>
+    <link rel="stylesheet" href="index.css">
+
 </head>
 <body>
+    <nav>
+        <div>
+            
+        </div>
+    </nav>
     
 </body>
 </html>
